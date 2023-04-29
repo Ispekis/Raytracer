1ac1098cf6808996a2525408d9b138b93cbc1077
Vincent Shao <vincent.shao@epitech.eu>
2023-04-29 13:43:11 +0200

    3 : feat : struct and op for vector3d

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+//
+// EPITECH PROJECT, 2023
+// B-OOP-400-PAR-4-1-raytracer-vincent.shao
+// File description:
+// main
+//
+
 mod error_handler;
 mod usage;
+mod raytracer;
+mod Vector3D;
 
 fn main() -> std::process::ExitCode {
     let args: Vec<String> = std::env::args().collect();
@@ -11,5 +20,12 @@ fn main() -> std::process::ExitCode {
     if error_handler::error_handler(&args) == 1 {
         return std::process::ExitCode::from(84);
     }
+    raytracer::run_raytracer();
+
+    let vec1 = Vector3D::Vector3D::new_value(2.0, 4.0, 0.0);
+    let vec2 = Vector3D::Vector3D::new_value(2.0, 4.0, 0.0);
+
+    // println!("{}", Vector3D::length(&vec1));
+    println!("{}", (vec1 / vec2).z);
     return std::process::ExitCode::SUCCESS;
 }
