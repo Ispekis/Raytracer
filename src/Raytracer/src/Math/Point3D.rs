e4fceffe8797b0831216caf293fbac8fd4bd8a7e
Vincent Shao <vincent.shao@epitech.eu>
2023-05-09 10:39:04 +0200

    dev:  feat : overload vector mult by f64, and cross, also overload print

--- src/Raytracer/src/Math/Point3D.rs
+++ src/Raytracer/src/Math/Point3D.rs
@@ -72,3 +72,9 @@ impl std::ops::Sub<Point3D> for Point3D{
         }
     }
 }
+
+impl std::fmt::Display for Point3D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Point3D(x={}, y={}, z={})", self.x, self.y, self.z)
+    }
+}
