3f0206a57200316f7d7f201ebb1b4eaf0404b5f2
Vincent Shao <vincent.shao@epitech.eu>
2023-05-10 13:28:21 +0200

    dev : feat : adding formulas for getting intersection's points

--- src/Raytracer/src/Math/formulas.rs
+++ src/Raytracer/src/Math/formulas.rs
@@ -5,6 +5,8 @@
 // formulas
 //
 
+use crate::Math::{Point3D::Point3D, Vector3D::Vector3D};
+
 pub fn compute_discriminant(a:f64, b:f64, c:f64) -> f64 {
     return b.powf(2.0) - 4.0 * a * c;
 }
@@ -23,4 +25,18 @@ pub fn resolve_quadratic_eq(delta:f64, a:f64, b:f64) -> Option<Vec<f64>>{
     res.push((-b - delta) / (2.0 * a));
 
     return Some(res);
+}
+
+pub fn get_inter_from_eq(eqs:Vec<f64>, point:Point3D, direction:Vector3D) -> Vec<Point3D>{
+    let mut res: Vec<Point3D> = Vec::new();
+    if eqs.len() == 1 {
+        res.push(Point3D::new(point.x + eqs[0] * direction.x, point.y + eqs[0] * direction.y, point.z + eqs[0] * direction.z));
+        return res;
+    }
+    if eqs.len() == 2 {
+        res.push(Point3D::new(point.x + eqs[1] * direction.x, point.y + eqs[1] * direction.y, point.z + eqs[1] * direction.z));
+        res.push(Point3D::new(point.x + eqs[1] * direction.x, point.y + eqs[1] * direction.y, point.z + eqs[1] * direction.z));
+        return res;
+    }
+    return res;
 }
