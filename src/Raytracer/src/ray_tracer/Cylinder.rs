d938cf6713d939fd2fd0c16424d5734dfb5a5d28
Yorennz ZELINA <yorennz.zelina@epitech.eu>
2023-05-11 12:41:16 +0200

    [ADD]cylinder

--- src/Raytracer/src/ray_tracer/Cylinder.rs
+++ src/Raytracer/src/ray_tracer/Cylinder.rs
@@ -28,7 +28,19 @@ impl Cylinder {
 }
 
 impl Primitives for Cylinder {
-    fn hits(&self, ray:Ray) -> bool{
+    fn hits(&self, ray:Ray) -> Option<Point3D> {
+        let dif = ray.origin - self.center;
+        let a = ray.direction.scal(&ray.direction);
+        let b = 2.0 * dif.scal(&ray.direction);
+        let c = dif.scal(&dif) - self.radius.powf(2.0);
+        let dis = formulas::compute_discriminant(a, b, c);
+        let res = formulas::resolve_quadratic_eq(dis, a, b);
+        if (res == None) {
+            return None;
+        } else {
+            let inter_points = formulas::get_inter_point_from_eq(res.unwrap(), ray.origin, ray.direction);
+            return Some(formulas::get_closest_point(inter_points, ray.origin));
+        }
         // let oc = ray.origin - self.center;
         // let a: f64 = ray.direction.scal(&ray.direction) - (ray.direction.scal(&oc).powf(2.0));
         // let b: f64 = 2.0 * (ray.direction.scal(&oc) - oc.scal(&ray.direction) * (oc.scal(&oc) - self.radius.powf(2.0).sqrt()));
@@ -48,39 +60,36 @@ impl Primitives for Cylinder {
         // }
         // return true;
 
-        let a = ray.direction.x.powi(2) + ray.direction.y.powi(2);
-        let b = 2.0 * (ray.direction.x * (ray.origin.x - self.center.x) + ray.direction.y * (ray.origin.y - self.center.y));
-        let c = (ray.origin.x - self.center.x).powi(2) + (ray.origin.y - self.center.y).powi(2) - self.radius.powi(2);
-        let discriminant = b.powi(2) - 4.0 * a * c;
+        // let a = ray.direction.x.powi(2) + ray.direction.y.powi(2);
+        // let b = 2.0 * (ray.direction.x * (ray.origin.x - self.center.x) + ray.direction.y * (ray.origin.y - self.center.y));
+        // let c = (ray.origin.x - self.center.x).powi(2) + (ray.origin.y - self.center.y).powi(2) - self.radius.powi(2);
+        // let discriminant = b.powi(2) - 4.0 * a * c;
 
-        if discriminant < 0.0 {
-            // return None;
-            return false;
-        }
+        // if discriminant < 0.0 {
+        //     return None;
+        // }
 
-        let t0 = (-b - discriminant.sqrt()) / (2.0 * a);
-        let t1 = (-b + discriminant.sqrt()) / (2.0 * a);
+        // let t0 = (-b - discriminant.sqrt()) / (2.0 * a);
+        // let t1 = (-b + discriminant.sqrt()) / (2.0 * a);
 
-        let mut t_min = f64::INFINITY;
-        let mut t_max = f64::NEG_INFINITY;
+        // let mut t_min = f64::INFINITY;
+        // let mut t_max = f64::NEG_INFINITY;
 
-        if t0 >= 0.0 && t0 <= self.height {
-            t_min = t_min.min(t0);
-            t_max = t_max.max(t0);
-        }
+        // if t0 >= 0.0 && t0 <= self.height {
+        //     t_min = t_min.min(t0);
+        //     t_max = t_max.max(t0);
+        // }
 
-        if t1 >= 0.0 && t1 <= self.height {
-            t_min = t_min.min(t1);
-            t_max = t_max.max(t1);
-        }
+        // if t1 >= 0.0 && t1 <= self.height {
+        //     t_min = t_min.min(t1);
+        //     t_max = t_max.max(t1);
+        // }
 
-        if t_min == f64::INFINITY || t_max == f64::NEG_INFINITY {
-            // return None;
-            return false;
-        }
+        // if t_min == f64::INFINITY || t_max == f64::NEG_INFINITY {
+        //     return None;
+        // }
 
         // Some(t_min)
-        return true;
     }
     fn translate(&mut self, Translate:Vector3D) {
         self.center.x += &Translate.x;
@@ -90,11 +99,16 @@ impl Primitives for Cylinder {
     fn rotateX(&mut self, angle:f64) {}
     fn rotateY(&mut self, angle:f64) {}
     fn rotateZ(&mut self, angle:f64) {}
+    fn suface_normal(&self, hit_point:Point3D) -> Vector3D {
+        let direction = (hit_point - self.center);
+        let norme = (direction.x * direction.x + direction.y * direction.y + direction.z * direction.z).sqrt();
+        return Vector3D::new(direction.x / norme, direction.y / norme, direction.z / norme)
+    }
 }
 
-impl Default for Cylinder {
+impl Default for Sphere {
     fn default() -> Self {
-        Cylinder {
+        Sphere {
             center: Point3D::default(),
             radius: 0.0,
             height: 0.0,
