82e8c9b60c39c15a5c10973f2f6e72732b9fdab4
Yorennz ZELINA <yorennz.zelina@epitech.eu>
2023-05-13 01:18:27 +0200

    30: fix: trying to rebase

--- src/Raytracer/src/config/fileconfig.rs
+++ src/Raytracer/src/config/fileconfig.rs
@@ -293,7 +293,8 @@ fn config_primitives(data:&Value) -> std::result::Result<Primitivest, Box<dyn st
 
     let cones = config_cones(data)?;
 
-    let cylinders = config_cylinders(data)?;
+    let cylinders = Vec::new();
+    // let cylinders = config_cylinders(data)?;
 
     Ok(Primitivest {spheres, planes, cones, cylinders})
 }
